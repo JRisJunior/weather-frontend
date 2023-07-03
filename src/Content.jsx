7c39303f519c6d9df9ede2f0534796c018ae29e9
JRisJunior <jeffmrubin@gmail.com>
2023-07-03 11:20:29 -0500

    display basic api data

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,24 @@
 import { WeatherIndex } from "./WeatherIndex";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export function Content() {
-  const weathers = [
-      {id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150},
-      {id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300},
-    ];
+  // const weathers = [
+  //     {id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150},
+  //     {id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300},
+  //   ];
+
+  const [weathers, setWeathers] = useState([]);
+
+  const handleIndexWeathers = () => {
+    console.log("handleIndexWeathers");
+    axios.get("https://api.weather.gov/points/41.8781,-87.6298").then((response) => {
+      console.log(response.data);
+      setWeathers(response.data);
+    });
+  };
+
+  useEffect(handleIndexWeathers, []);
 
   return (
     <div>
--- src/WeatherIndex.jsx
+++ src/WeatherIndex.jsx
@@ -2,14 +2,7 @@ export function WeatherIndex(props) {
   return (
     <div>
       <h1>All weather</h1>
-      {props.weathers.map((weather) => (
-        <div key={weather.id}>
-          <h2>{weather.name}</h2>
-          <img src={weather.url} />
-          <p>Width: {weather.width}</p>
-          <p>Height: {weather.height}</p>
-        </div>
-       ))}
+      {props.weathers.id}
     </div>
   );
 }
