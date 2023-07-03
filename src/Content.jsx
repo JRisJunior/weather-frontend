a2f20ae93b4ae546fd373ed3606bd74d0b34bba0
JRisJunior <jeffmrubin@gmail.com>
2023-07-03 10:44:18 -0500

    placeholder data added

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,14 @@
+import { WeatherIndex } from "./WeatherIndex";
+
 export function Content() {
+  const weathers = [
+      {id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150},
+      {id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300},
+    ];
+
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <WeatherIndex weathers={weathers} />
     </div>
   )
 }
--- src/WeatherIndex.jsx
+++ src/WeatherIndex.jsx
@@ -1,7 +1,15 @@
-export function WeatherIndex() {
+export function WeatherIndex(props) {
   return (
     <div>
       <h1>All weather</h1>
+      {props.weathers.map((weather) => (
+        <div key={weather.id}>
+          <h2>{weather.name}</h2>
+          <img src={weather.url} />
+          <p>Width: {weather.width}</p>
+          <p>Height: {weather.height}</p>
+        </div>
+       ))}
     </div>
   );
 }
