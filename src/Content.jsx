2bafcee14a2477c1235ca1d9fefcfd3212e13405
JRisJunior <jeffmrubin@gmail.com>
2023-07-03 13:51:46 -0500

    weatherindex broken

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,6 @@
-import { WeatherIndex } from "./WeatherIndex";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { WeatherIndex } from "./WeatherIndex";
 
 export function Content() {
   // const weathers = [
@@ -8,13 +8,34 @@ export function Content() {
   //     {id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300},
   //   ];
 
-  const [weathers, setWeathers] = useState([]);
+  const [weathers, setWeathers] = useState([
+  {id: null,
+  type: null,
+  cwa: null,
+  fireWeatherZone: null,
+  forecast: null,
+  forecastGridData: null,
+  forecastHourly: null,
+  forecastOffice: null,
+  forecastZone: null,
+  gridId: null,
+  gridX: null,
+  gridY: null,
+  observationStations: null,
+  radarStation: null,
+  relativeLocation: {
+    type: null,
+    geometry: null,
+    properties: null
+  },
+  timeZone: null
+}]);
 
   const handleIndexWeathers = () => {
     console.log("handleIndexWeathers");
     axios.get("https://api.weather.gov/points/41.8781,-87.6298").then((response) => {
-      console.log(response.data);
-      setWeathers(response.data);
+      console.log(response.data.properties);
+      setWeathers(response.data.properties);
     });
   };
 
--- src/WeatherIndex.jsx
+++ src/WeatherIndex.jsx
@@ -1,8 +1,22 @@
 export function WeatherIndex(props) {
+
+  console.log("props:", props);
+
   return (
     <div>
       <h1>All weather</h1>
-      {props.weathers.id}
+      {props.weathers}
     </div>
   );
 }
+
+
+
+// +       {props.photos.map((photo) => (
+//   +         <div key={photo.id}>
+//   +           <h2>{photo.name}</h2>
+//   +           <img src={photo.url} />
+//   +           <p>Width: {photo.width}</p>
+//   +           <p>Height: {photo.height}</p>
+//   +         </div>
+//   +       ))}
